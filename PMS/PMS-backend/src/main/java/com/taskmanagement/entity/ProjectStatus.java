89cb989d4df05deda86eb623f9bf84a205eb42cc
Sakshi <sakshinagare137@gmail.com>
2024-08-27 09:48:18 +0530

    Create ProjectStatus.java

---
+++ PMS/PMS-backend/src/main/java/com/taskmanagement/entity/ProjectStatus.java
@@ -0,0 +1,33 @@
+package com.taskmanagement.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class ProjectStatus {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "project_id")
+	private Project project;
+
+	private String status;
+
+	private String addedTime;
+
+	private Integer employeeId;
+
+}
